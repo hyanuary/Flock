82de6a1b7b5b55b42d5b05e212873e3d1c1b5136
Henoch Yanuary <1003495@student.sae.edu.au>
2017-05-04 15:58:25 +0900

    finalizing the constraints using loop

--- Assets/Constraints.cs
+++ Assets/Constraints.cs
@@ -21,6 +21,20 @@ public class Constraints : MonoBehaviour {
             
         }
     }
+    void constrain_3d_fixed_dist1(ref Vector3 pos2, ref Vector3 pos3,
+        float desired_dist, float compensate1, float compensate2)
+    {
+        Vector3 delta = pos3 - pos2;
+        float deltalength = (float)Mathf.Sqrt(delta.x * delta.x + delta.y * delta.y + delta.z * delta.z);
+        if (deltalength > 0)
+        {
+            float diff = (deltalength - desired_dist) / deltalength;
+            pos2 += delta * compensate1 * diff;
+            pos3 -= delta * compensate2 * diff;
+
+
+        }
+    }
 
     // Use this for initialization
     void Start()
@@ -34,9 +48,12 @@ public class Constraints : MonoBehaviour {
 
         Vector3 p1 = obj1.transform.position;
         Vector3 p2 = obj2.transform.position;
+        Vector3 p3 = obj3.transform.position;
         constrain_3d_fixed_dist(ref p1, ref p2,3.0f, 0.5f, 0.5f);
+        constrain_3d_fixed_dist1(ref p2, ref p3, 3.0f, 0.5f, 0.5f);
         obj1.transform.position = p1;
         obj2.transform.position = p2;
+        obj3.transform.position = p3;
 
     }
 }
---
+++ Assets/Constraints2.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Constraints2 : MonoBehaviour {
+
+    public int numBox = 5;
+    public GameObject[] boxes;
+
+    void constrain_3d_fixed_dist(ref Vector3 pos1, ref Vector3 pos2,
+       float desired_dist, float compensate1, float compensate2)
+    {
+        Vector3 delta = pos2 - pos1;
+        float deltalength = (float)Mathf.Sqrt(delta.x * delta.x + delta.y * delta.y + delta.z * delta.z);
+        if (deltalength > 0)
+        {
+            float diff = (deltalength - desired_dist) / deltalength;
+            pos1 += delta * compensate1 * diff;
+            pos2 -= delta * compensate2 * diff;
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        for(int i = 0; i < numBox - 1; i++ )
+        {
+            Vector3 p1 = boxes[i].transform.position;
+            Vector3 p2 = boxes[i + 1].transform.position;
+
+            constrain_3d_fixed_dist(ref p1, ref p2, 2.0f, 1.0f, 0.5f);
+
+            boxes[i].transform.position = p1;
+            boxes[i + 1].transform.position = p2;
+     
+        }
+		
+	}
+}
